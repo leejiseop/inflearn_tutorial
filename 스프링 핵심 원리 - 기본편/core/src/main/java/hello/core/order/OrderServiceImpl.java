858d44201c3b52a68113122099abd6a55b49dc90
leejiseop <lkj0245@naver.com>
2024-04-23 01:25:36 +0900

    여러 개의 빈 받아오기

--- 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,10 +1,12 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,7 +22,7 @@ public class OrderServiceImpl implements OrderService{
 
 //    lombok 의 @RequiredArgsConstructor 로 대체가능
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) { // 뭐든 인터페이스에 맞는 구현체를 넣어주겠지!
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) { // 뭐든 인터페이스에 맞는 구현체를 넣어주겠지!
         this.memberRepository = memberRepository; // 오로지 인터페이스에만 의존!
         this.discountPolicy = discountPolicy; // DIP 원칙을 지킨다
     }
