100195aeae418ee2ce3b6603899bd86d6e5aae57
leejiseop <lkj0245@naver.com>
2024-06-16 02:18:54 +0900

    커밋 정리

--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa`/src/main/java/hellojpa/JpaMain.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa`/src/main/java/hellojpa/JpaMain.java
@@ -2,28 +2,41 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
 
+        // 딱 하나만 생성
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager(); // 트랜잭션 단위로 만들어서 사용
+        // 고객 요청이 올 때 마다 생성, 트랜잭션 단위로 만들어서 사용 -> close
+        // 절대로!! 쓰레드 간 공유하면 안된다!!
+        // em 은 사용하고 버리는 것
+        EntityManager em = emf.createEntityManager();
         //code
 
         // tx start
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        Member member = new Member();
-        member.setId(1L);
-        member.setName("HelloA");
+        try {
+            Member findmember1 = em.find(Member.class, 101L);
+            System.out.println("findmember1 = " + findmember1);
+            em.clear();
+            Member findmember2 = em.find(Member.class, 101L);
+            System.out.println("findmember2 = " + findmember2);
+            findmember2 = em.find(Member.class, 101L);
+            System.out.println("findmember2 = " + findmember2);
 
-        em.persist(member);
+            // tx commit
+            tx.commit(); // commit 시점에 변경점을 감지하여 update 한다
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close(); // 안닫으면 db connection 을 계속 물고있다
+        }
 
-        // tx commit
-        tx.commit();
-
-        em.close();
-        emf.close();
+        emf.close(); // 전제 애플리케이션이 끝나면 팩토리도 닫아준다 -> WAS 가 내려갈 때
     }
 }
--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa`/src/main/java/hellojpa/Member.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa`/src/main/java/hellojpa/Member.java
@@ -1,13 +1,26 @@
 package hellojpa;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+//@Table(name = "USER") // DB 테이블명이 다르다면 이렇게
 public class Member {
     @Id
     private  Long id;
+
+//    @Column(name = "username") // DB 컬럼명이 다르다면 이렇게
     private String name;
+
+    public Member() {
+    }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public Long getId() {
         return id;
